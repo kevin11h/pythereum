32ccc80e196d686801a80e77a3d98d63f3079378
Rashid Hasan <nhmrashid@gmail.com>
2018-11-19 00:20:16 -0500

    Added wordlist for wallet generation

--- pythereum/wallet.py
+++ pythereum/wallet.py
@@ -1,18 +1,23 @@
+import os
 import base64
 import hashlib
-import secrets
+import random
 import binascii
+from pathlib import Path
 from typing import Dict
 
 from pythereum.ecdsa import SigningKey
 from pythereum.ecdsa.util import PRNG
 
+SEED_WORD_COUNT = 24
+ROOT_DIR = Path(os.path.dirname(__file__))
+
 
 def create_wallet(*seed) -> Dict[str, str]:
-    if seed:
-        rng = PRNG(''.join(seed))
-    else:
-        rng = PRNG(secrets.token_hex())
+    if not seed:
+        with open(str(ROOT_DIR / "seed_wordlist")) as seed_file:
+            seed = random.sample(seed_file.read().split("\n"), SEED_WORD_COUNT)
+    rng = PRNG(''.join(seed))
     private_key = SigningKey.generate(entropy=rng)
     public_key = private_key.get_verifying_key()
 
@@ -25,7 +30,7 @@ def create_wallet(*seed) -> Dict[str, str]:
 
     return {"public_key": public_key.to_pem().decode("utf-8"),
             "private_key": private_key.to_pem().decode("utf-8"),
-            "address": address}
+            "address": address, "seeds": list(seed)}
 
 
 def sign_item(private_key, item):
