0c0afa63e39658cc00adefc984242bda4d37e1c5
Rashid Hasan <nhmrashid@gmail.com>
2018-11-18 23:21:58 -0500

    Added external accounts

---
+++ pythereum/accounts/external_account.py
@@ -0,0 +1,34 @@
+import base64
+import hashlib
+import secrets
+import binascii
+from typing import Dict
+
+from .ecdsa import SigningKey
+from .ecdsa.util import PRNG
+
+
+def create_wallet(*seed) -> Dict[str, str]:
+    if seed:
+        rng = PRNG(''.join(seed))
+    else:
+        rng = PRNG(secrets.token_hex())
+    private_key = SigningKey.generate(entropy=rng)
+    public_key = private_key.get_verifying_key()
+
+    pem_pub_key = public_key.to_pem()
+    hash_pub_key = hashlib.sha256(pem_pub_key).hexdigest()
+    ripe_pub_key = hashlib.new("ripemd160")
+    ripe_pub_key.update(hash_pub_key.encode())
+    hash_pub_key = hashlib.sha256(hashlib.sha256(ripe_pub_key.hexdigest().encode()).hexdigest().encode())
+    address = base64.b64encode(hash_pub_key.hexdigest().encode()).decode("utf-8")[:-2]
+
+    return {"public_key": public_key.to_pem().decode("utf-8"),
+            "private_key": private_key.to_pem().decode("utf-8"),
+            "address": address}
+
+
+def sign(private_key, item):
+    private_key = SigningKey.from_pem(private_key)
+    signed_item = private_key.sign_deterministic(item.encode())
+    return binascii.b2a_base64(signed_item, newline=False).decode("utf-8")
