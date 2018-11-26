46dccf43d44fcd6896b264559032f50ae4a71b50
amitri1234 <45216903+amitri1234@users.noreply.github.com>
2018-11-26 17:05:47 -0500

    Update mempool.py

--- pythereum/mempool.py
+++ pythereum/mempool.py
@@ -8,13 +8,16 @@ class Mempool:
     messages = {}
 
     @classmethod
+    # Add transaction to transaction dictionary
     def add_transaction(cls, transaction):
         if isinstance(transaction, dict):
             cls.transactions[transaction["txid"]] = transaction
         else:
             cls.transactions[transaction.txid] = transaction.jsonify()
 
+            
     @classmethod
+    # Add contract to contracts dictionary
     def add_contract(cls, contract):
         if isinstance(contract, dict):
             cls.contracts[contract["cxid"]] = contract
@@ -22,6 +25,7 @@ class Mempool:
             cls.transactions[contract.cxid] = contract.jsonify()
 
     @classmethod
+    # Add message to messages dictionary
     def add_message(cls, message):
         if isinstance(message, dict):
             cls.messages[message["mxid"]] = message
@@ -29,6 +33,7 @@ class Mempool:
             cls.messages[message.mxid] = message.jsonify()
 
     @classmethod
+    # Pop transactions from transactions dictionary
     def pop_transactions(cls, n=5):
         for txid, tx in cls.transactions.items():
             if time.time() - tx["time"] > 300:
@@ -44,6 +49,7 @@ class Mempool:
         return txs
 
     @classmethod
+    # Pop contracts from contracts dictionary
     def pop_contracts(cls, n=5):
         for cxid, cx in cls.contracts.items():
             if time.time() - cx["time"] > 300:
@@ -59,6 +65,7 @@ class Mempool:
         return cxs
 
     @classmethod
+    # Pop messages from messages dictionary
     def pop_messages(cls, n=5):
         for mxid, mx in cls.messages.items():
             if time.time() - mx["time"] > 300:
